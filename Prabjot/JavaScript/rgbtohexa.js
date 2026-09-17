30dff58e30885d4fabeb42383642c66294f41923
Prabjot <prabjot@example.com>
2026-09-17 11:44:22 +0530

    Add Prabjot tasks into folder

---
+++ Prabjot/JavaScript/rgbtohexa.js
@@ -0,0 +1,8 @@
+function rgbToHex(r, g, b) {
+    return "#" +
+        r.toString(16).padStart(2, "0") +
+        g.toString(16).padStart(2, "0") +
+        b.toString(16).padStart(2, "0");
+}
+
+console.log(rgbToHex(255, 99, 71));
